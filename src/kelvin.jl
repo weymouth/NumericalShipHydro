437a28d269f74d74ad5e920b5d8fb9774dfdbabb
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2024-01-10 17:23:50 +0100

    Working green's function
    Having trouble getting AD to work for the derivatives, however.

--- src/kelvin.jl
+++ src/kelvin.jl
@@ -1,15 +1,18 @@
-function R(x,x₀) # rankine source/image pair
-    x₀⁻ = x₀*SA[1,1,-1] # reflect z-value
-    return 1/len(x-x₀⁻)-1/len(x-x₀)
+using StaticArrays
+function G(x,x₀;k=1)
+    # Wavenumber-scaled distance from source and image source
+    r = k*(x-x₀); r′ = k*(x-x₀.*SA[1,1,-1])
+    # Rankine and wave potentials
+    return 1/hypot(r...)-1/hypot(r′...)+D(r′...)+W(r′...)
 end
-len(x) = √(x'*x)
 
-using QuadGK,Plots,SpecialFunctions
-D(x,y,z) = quadgk_count(T->Di(x,y,z,T),-Inf,Inf,rtol=1e-5)
-Di(x,y,z,T) = 2/π*real(SpecialFunctions.expintx(χ(x,y,z,T)))
+using QuadGK,SpecialFunctions
+# Local non-oscillatory wave
+D(x,y,z) = 2/π*quadgk(T->Di(x,y,z,T),-Inf,Inf,rtol=1e-5)[1]
+Di(x,y,z,T) = real(expintx(χ(x,y,z,T)))
 χ(x,y,z,T) = complex((1+T^2)*z,(x+y*T)*hypot(1,T))
-W(x,y,z) = quadgk_count(T->Wi(x,y,z,T),x/abs(y),√(-5log(10)/z-1),rtol=1e-5)
-Wi(x,y,z,T) = 4exp((1+T^2)*z)*sin((x-abs(y)*T)*hypot(1,T))
+# Far-field oscillatory wave
+W(x,y,z) = 4quadgk(T->Wi(x,y,z,T),x/abs(y),√(-5log(10)/z-1),rtol=1e-5)[1]
+Wi(x,y,z,T) = exp((1+T^2)*z)*sin((x-abs(y)*T)*hypot(1,T))
 
-D(-0.1,0.2,-0.01)
-W(-0.1,0.2,-0.01)
+G(SA[0.1,0.2,0],SA[0,0,-1])
