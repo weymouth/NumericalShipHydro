c9cfaeb7915098b5f3893a7e6a93ed4b1958d687
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2024-01-11 13:52:19 +0100

    Add adaptive tolerances and error check

--- src/kelvin.jl
+++ src/kelvin.jl
@@ -1,22 +1,26 @@
 using StaticArrays
-function G(x,x₀;k=1)
+function G(x,x₀;k=1,kwargs...)
     # Wavenumber-scaled distance from source and image source
     r = k*(x-x₀); r′ = k*(x-x₀.*SA[1,1,-1])
     # Rankine and wave potentials
-    return 1/hypot(r...)-1/hypot(r′...)+D(r′...)+W(r′...)
+    return 1/hypot(r...)-1/hypot(r′...)+D(r′...;kwargs...)+W(r′...;kwargs...)
 end
 
 using QuadGK,SpecialFunctions
 # Local non-oscillatory wave
-D(x,y,z) = 2/π*quadgk(T->Di(x,y,z,T),-Inf,Inf,rtol=1e-5)[1]
+D(x,y,z;atol=1e-3,Tmax=Inf) = 2/π*quadgk(T->Di(x,y,z,T),-Tmax,Tmax;atol)[1]
 Di(x,y,z,T) = real(expintx(χ(x,y,z,T)))
 χ(x,y,z,T) = complex((1+T^2)*z,(x+y*T)*hypot(1,T))
 # Far-field oscillatory wave
-W(x,y,z) = 4quadgk(T->Wi(x,y,z,T),x/abs(y),√(-5log(10)/z-1),rtol=1e-5)[1]
+W(x,y,z;atol=1e-3,Tmax=√(log(atol/10)/z-1)) = 4quadgk(T->Wi(x,y,z,T),x/abs(y),Tmax;atol)[1]
 Wi(x,y,z,T) = exp((1+T^2)*z)*sin((x-abs(y)*T)*hypot(1,T))
 
 using FiniteDifferences
 # Potential and velocity
-ϕ(x) = G(x,SA[0,0,-1]) # replace with summation over points
-u(x) = grad(central_fdm(2, 1),ϕ,x)[1]
-u(SA[0.1,0.2,0])
+ϕ(x;kwargs...) = G(x,SA[0,0,-1];kwargs...) # replace with summation over points
+u(x;kwargs...) = grad(central_fdm(2, 1),x->ϕ(x;kwargs...),x)[1]
+
+# Check that errors are quite small 
+for x in (SA[0.1,0.1,-0.9],SA[1.,1.,0.],SA[10.,10.,0.])
+    println(sum(abs2,u(x).-u(x,atol=1e-16,Tmax=Inf)))
+end
