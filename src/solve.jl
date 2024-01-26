3b288001a82df5d9e8360d7e80310f329fca850b
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2024-01-26 16:29:14 +0100

    Sort of working
    It's working ok, but the stagnation velocity isn't right and the added-mass is 7% off with 1k panels. I might need to go to the quadrapole level.

--- src/solve.jl
+++ src/solve.jl
@@ -1,5 +1,12 @@
-G(x,a) = inv(hypot(x-a...))
-∇G(x,a,G) = gradient(x->G(x,a),x)
-uₙ(pᵢ,pⱼ,G) = pᵢ["x"]==pⱼ["x"] ? 0.5π : ∇G(pᵢ["x"],pⱼ["x"],G)⋅pᵢ["n"]*pⱼ["dA"]
+source(x,a) = inv(hypot(x-a...))
+∇G(x,a;G=source) = gradient(x->G(x,a),x)
+∂ₙG(pᵢ,pⱼ;G=source) = pᵢ==pⱼ ? 2π : ∇G(pᵢ["x"],pⱼ["x"];G)⋅pᵢ["n"]*pⱼ["dA"]
 Uₙ(pᵢ;U=[1,0,0]) = U⋅pᵢ["n"]
-u(x,q,panels,G;U=[1,0,0]) = U+sum(qᵢ*∇G(x,pᵢ["x"],G)*pᵢ["dA"] for (qᵢ,pᵢ) in zip(q,panels))
+function body_velocity(q,panels;U=[1,0,0],G=source)
+    u(p) = U-Uₙ(p;U)*p["n"]+sum(qᵢ*∇G(p["x"],pᵢ["x"];G)*pᵢ["dA"] for (qᵢ,pᵢ) in zip(q,panels) if p≠pᵢ)
+    stack(u.(panels))
+end
+function added_mass(q,panels;G=source)
+    ϕ(x) = sum(qᵢ*G(x,pᵢ["x"])*pᵢ["dA"] for (qᵢ,pᵢ) in zip(q,panels) if x≠pᵢ["x"])
+    -sum(ϕ(pᵢ["x"])*pᵢ["n"]*pᵢ["dA"] for pᵢ in panels)
+end
--- src/sphere.jl
+++ src/sphere.jl
@@ -1,20 +1,27 @@
 include("util.jl")
 using Plots
-function sphere(n,m;a=1)
-    ϕ = range(0,π,length=n+1)[1:n]; dϕ = ϕ[2]
-    θ = range(0,2π,length=m+1)[1:m]; dθ = θ[2]
+function sphere(h;a=1)
     x(ϕ,θ) = a .* [cos(θ)*sin(ϕ),sin(θ)*sin(ϕ),cos(ϕ)]
-    p = @. param_props(x,ϕ'+0.5dϕ,θ+0.5dθ,dϕ*dθ)
-    return reshape(p,:),stack(get.(p,"x",0))
+    dϕ = π/round(π*a/h)
+    mapreduce(vcat,0.5dϕ:dϕ:π) do ϕ
+        dθ = 2π/round(2π*a*sin(ϕ)/h)
+        param_props.(x,ϕ,0.5dθ:dθ:2π,dϕ*dθ)
+    end
 end
-panels,X = sphere(10,20,a=1);
-scatter(X[1,:,:],X[2,:,:],X[3,:,:],camera=(-20,20),legend=false)
-sum(get.(panels,"dA",0))/4π-1
+h = 0.1; panels = sphere(h)
+x,y,z = eachrow(stack(get.(panels,"x",0)))
+plot(x,y,z,camera=(-20,20),legend=false)
+dA = get.(panels,"dA",0)
+sum(dA)/4π-1
+plot(X[3,:],dA/h^2,ylim=(0,2),xlabel="z",ylabel="dA/h²",legend=false)
 
 include("solve.jl")
-A = uₙ.(panels,permutedims(panels),ϕ)
-U = [0,0,1]
-b = -Uₙ.(panels;U)
+A = ∂ₙG.(panels,permutedims(panels))
+b = -Uₙ.(panels)
 q = A \ b
-A*q≈b
-plot(-10:0.1:-1,x->u([0,0,x],q,panels,G;U)[3])
+A*q ≈ b
+ma = added_mass(q,panels)
+ma[1]/(2π/3)-1
+u,v,w = eachrow(body_velocity(q,panels))
+equator = filter(i->abs(z[i])<h,1:length(panels))
+quiver(x[equator],y[equator],quiver = (u[equator],v[equator]), aspect_ratio=:equal)
