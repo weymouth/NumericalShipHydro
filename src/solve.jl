7c73fb18dbd9c6572aa47ca23dc4184a7887a21f
weymouth <gabriel.weymouth@gmail.com>
2024-01-30 21:07:43 +0100

    Switch solve to kwargs

--- src/solve.jl
+++ src/solve.jl
@@ -1,7 +1,7 @@
 using LinearAlgebra: ×,⋅
 using TypedTables
 include("util.jl")
-source(x) = -inv(hypot(x...))
+source(x,a) = -1/hypot(x-a...)
 """
     param_props(S,ξ₁,ξ₂,dξ₁,dξ₂) -> (x,n̂,dA,T₁,T₂)
 
@@ -18,32 +18,32 @@ end
 ξgl,ωgl = gausslegendre(2)./2 # use an even power to avoid ξ=0
 quadξ(f;x=ξgl,w=ωgl) = quadgl(f;x,w) # integrate over ξ=[-0.5,0.5]
 """
-    ϕ(x,p;G=source)
-    ∇ϕ(x,p;G=source)
+    ϕ(x,p;G=source,kwargs...)
+    ∇ϕ(x,p;kwargs...)
 
-Approximate influence `ϕ(x) = ∫ₚ G(x-x')ds'` over panel `p`'s and it's gradient
+Approximate influence `ϕ(x) = ∫ₚ G(x,x')ds'` over panel `p`'s and it's gradient
 `∇ϕ` using automatic-differentiation, except at `x==p.x`, where `∇ϕ=2πn̂`.
 """
-@fastmath function ϕ(x,p;G=source)
-    sum(abs2,x-p.x)>9p.dA && return p.dA*G(x-p.x) # single-point quadrature
-    p.dA*quadξ(ξ₁->quadξ(ξ₂->G(x-p.x-ξ₁*p.T₁-ξ₂*p.T₂)))
+@fastmath function ϕ(x,p;G=source,kwargs...)
+    sum(abs2,x-p.x)>9p.dA && return p.dA*G(x,p.x;kwargs...) # single-point quadrature
+    p.dA*quadξ(ξ₁->quadξ(ξ₂->G(x,p.x+ξ₁*p.T₁+ξ₂*p.T₂;kwargs...)))
 end
-∇ϕ(x,p;G=source) = x==p.x ? 2π*p.n : gradient(x->ϕ(x,p;G),x)
+∇ϕ(x,p;kwargs...) = x==p.x ? 2π*p.n : gradient(x->ϕ(x,p;kwargs...),x)
 """ 
-    ∂ₙϕ(pᵢ,pⱼ;G=source) = A
+    ∂ₙϕ(pᵢ,pⱼ;kwargs...) = A
 
 Normal derivative of the influence of panel `pⱼ` on `pᵢ`.
 """
-∂ₙϕ(pᵢ,pⱼ;G=source) = ∇ϕ(pᵢ.x,pⱼ;G) ⋅ pᵢ.n
+∂ₙϕ(pᵢ,pⱼ;kwargs...) = ∇ϕ(pᵢ.x,pⱼ;kwargs...) ⋅ pᵢ.n
 Uₙ(pᵢ;U=[1,0,0]) = U ⋅ pᵢ.n
 """
-    φ(x,q,panels;G=source)
-    ∇φ(x,q,panels;G=source)
+    φ(x,q,panels;kwargs...)
+    ∇φ(x,q,panels;kwargs...)
 
 The velocity potential `φ(x) = ∫ₛ q(x')G(x-x')ds' = ∑ᵢqᵢϕ(x,pᵢ)` induced by an
 array of `panels` with strength `q`, and its gradient `∇φ(x) = ∑ᵢqᵢ∇ϕ(x,pᵢ)`.
 """
-φ(x,q,panels;G=source) = sum(qᵢ*ϕ(x,pᵢ;G) for (qᵢ,pᵢ) in zip(q,panels)) 
-∇φ(x,q,panels;G=source) = sum(qᵢ*∇ϕ(x,pᵢ;G) for (qᵢ,pᵢ) in zip(q,panels))
-body_velocity(q,panels;U=[1,0,0],G=source) = map(x->U+∇φ(x,q,panels;G),panels.x) |> stack
-added_mass(q,panels;G=source) = sum(p->φ(p.x,q,panels;G)*p.n*p.dA,panels)
+φ(x,q,panels;kwargs...) = sum(qᵢ*ϕ(x,pᵢ;kwargs...) for (qᵢ,pᵢ) in zip(q,panels)) 
+∇φ(x,q,panels;kwargs...) = sum(qᵢ*∇ϕ(x,pᵢ;kwargs...) for (qᵢ,pᵢ) in zip(q,panels))
+body_velocity(q,panels;U=[1,0,0],kwargs...) = map(x->U+∇φ(x,q,panels;kwargs...),panels.x) |> stack
+added_mass(q,panels;kwargs...) = sum(p->φ(p.x,q,panels;kwargs...)*p.n*p.dA,panels)
