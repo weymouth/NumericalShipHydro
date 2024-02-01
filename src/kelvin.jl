dbddc0e5f16ec051d2150a86eb3617950fb62f65
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2024-02-01 17:29:55 +0100

    Update kelvin
    Updated the kelvin function into a (mostly) single function and corrected the scaling. I also added documentation and improved the plot. Results are looking very good for the source potential as a function of Fn.

--- src/kelvin.jl
+++ src/kelvin.jl
@@ -1,32 +1,32 @@
-include("util.jl")
-function kelvin(x,a;k=1,kwargs...)
-    # Wavenumber-scaled vector from source and image source
-    r = k * (x-a); r′ = k * (x-a .* [1,1,-1])
-    # Rankine and wave potentials
-    return 1/hypot(r...)-1/hypot(r′...)+GD(r′...;kwargs...)+GW(r′...;kwargs...)
+include("solve.jl")
+"""
+    kelvin(ξ,a;Fn=1,kwargs...)
+
+Green Function `G(ξ)` for a source at position `α` moving with `Fn≡U/√gL` below 
+the free-surface. This function assumes that the free surface location is ζ=0 
+and the apparent velocity direction is Û=[-1,0,0]. See Noblesse 1981 for details.
+"""
+function kelvin(ξ,α;Fn=1,kwargs...)
+    α[3] ≥ 0 && throw(DomainError(α[3],"Source must be below the free surface at ζ=0"))
+    # Froude number scaled distances from the source's image
+    x,y,z = (ξ-α .* [1,1,-1])/Fn^2;
+    # Near-field image and non-oscillatory wave
+    N = 1/hypot(x,y,z)+2/π*quadgl_inf(T->Ni(x,y,z,T);kwargs...)
+    # Far-field oscillatory wave
+    b = √max(-5log(10)/z-1,0); a = max(x/abs(y),-b) # integration limits
+    W = ifelse(a≥b || x==y==0, 0., 4*quadgl_ab(T->Wi(x,y,z,T),a,b;kwargs...))
+    # Total Green function
+    return source(ξ,α)+(N+W)/Fn^2
 end
-
-# Near-field non-oscillatory wave
-Di(x,y,z,T) = real(expintx(complex((1+T^2)*z,eps(T)+(x+y*T)*hypot(1,T))))
-GD(x,y,z;kwargs...) = 2/π*quadgl_inf(T->Di(x,y,z,T);kwargs...)
-
-# Far-field oscillatory wave
+Ni(x,y,z,T) = real(expintx(complex((1+T^2)*z,eps(T)+(x+y*T)*hypot(1,T))))
 Wi(x,y,z,T) = exp((1+T^2)*z)*sin((x-abs(y)*T)*hypot(1,T))
-function GW(x,y,z;kwargs...)
-    a,b = x/abs(y),√(-5log(10)/z-1) # integral domain
-    (a≥b || x==y==0) ? 0. : 4*quadgl_ab(T->Wi(x,y,z,T),max(a,-b),b;kwargs...)
-end
 
-# Surface elevation plots
+# Surface elevation ζ/Fn²
 ζ(x,y,q,panels;kwargs...) = derivative(x->φ([x,y,0],q,panels;kwargs...),x)
-
-using Plots
-function source_elevation_plot(a=[0,0,-1],k=1)
-    ζ(x,y) = derivative(x->kelvin([x,y,0],a;k),x) # unit-point source
-    x,y = -15:0.1:3,-6.05:0.1:6.05
-    contour(x,y,ζ,aspect_ratio=:equal,levels=[-2.5,-2,-1.5,-1,-.5,.5,1,1.5],legend=false)
-end
-function elevation_plot(q,panels;kwargs...)
-    x,y = -15:0.1:3,-6.05:0.1:6.05
-    contour(x,y,(x,y)->ζ(x,y,q,panels;kwargs...),aspect_ratio=:equal,levels=[-2.5,-2,-1.5,-1,-.5,.5,1,1.5],legend=false)
+function source_elevation_plot(;Fn=1) # test function 
+    ζ(x,y) = derivative(x->kelvin([x,y,0],[0,0,-1];Fn),x) # unit-point source
+    x,y = -15:0.1:3,-6:0.1:6; z = ζ.(x',y)
+    mn,mx = round.(extrema(z),digits=1); 
+    contour(x,y,z/max(-mn,mx),levels=filter(≠(0),-0.9:0.15:0.9),aspect_ratio=:equal,
+        title="Point-source wave elevation, Fn=$Fn, ζ/Fn²=[$mn,$mx]",legend=false)
 end
