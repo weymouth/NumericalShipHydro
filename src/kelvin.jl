1e5121fb88c94ccdf470f4f3549b310ab5e5c3f4
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2024-02-02 17:55:39 +0100

    Partial N fix
    Tracked down the problem to the N quadrature. There is a discontinuous derivative in Ni @ x+y*T=0, which leads to noise. I have reduced the problem by shifting the discontinuity to the center, but the shift is x/y, which gets too big. I should also adjust the scale.

--- src/kelvin.jl
+++ src/kelvin.jl
@@ -9,9 +9,10 @@ and the apparent velocity direction is Û=[-1,0,0]. See Noblesse 1981 for detai
 function kelvin(ξ,α;Fn=1,kwargs...)
     α[3] ≥ 0 && throw(DomainError(α[3],"Source must be below the free surface at ζ=0"))
     # Froude number scaled distances from the source's image
-    x,y,z = (ξ-α .* [1,1,-1])/Fn^2;
+    x,y,z = (ξ-α .* [1,1,-1])/Fn^2
     # Near-field image and non-oscillatory wave
-    N = 1/hypot(x,y,z)+2/π*quadgl_inf(T->Ni(x,y,z,T);kwargs...)
+    T₀ = ifelse(y^2<x^2,0,x/y) # integration center
+    N = 1/hypot(x,y,z)+2/π*quadgl_inf(T->Ni(x,y,z,T-T₀);kwargs...)
     # Far-field oscillatory wave
     b = √max(-5log(10)/z-1,0); a = max(x/abs(y),-b) # integration limits
     W = ifelse(a≥b || x==y==0, 0., 4*quadgl_ab(T->Wi(x,y,z,T),a,b;kwargs...))
@@ -21,6 +22,22 @@ end
 Ni(x,y,z,T) = real(expintx(complex((1+T^2)*z,eps(T)+(x+y*T)*hypot(1,T))))
 Wi(x,y,z,T) = exp((1+T^2)*z)*sin((x-abs(y)*T)*hypot(1,T))
 
+using Plots
+xgl,wgl = gausslegendre(61);
+plt = plot(ylims=(-3,2))
+for y = [0,0.1,1,10]
+    plot!(plt,-10:0.01:3,x->derivative(x->kelvin([x,y,0],[0,0,-1];Fn=0.6,xgl,wgl),x),label=y)
+end
+display(plt)
+
+x,y,z,Fn = -.5,.1,0,0.6
+xgl,wgl = gausslegendre(21);
+derivative(x->kelvin([x,y,0],[0,0,-1];Fn=0.6,xgl,wgl),x) # should be ~ -1
+
+x,y,z = -1.3888,2.777,-2.777
+T₀ = x/hypot(z,y) # integration center
+plot(-10:0.1:10,T->Ni(x,y,z,T-T₀))
+
 # Surface elevation ζ/Fn²
 ζ(x,y,q,panels;kwargs...) = derivative(x->φ([x,y,0],q,panels;kwargs...),x)
 function source_elevation_plot(;Fn=1) # test function 
@@ -29,4 +46,12 @@ function source_elevation_plot(;Fn=1) # test function
     mn,mx = round.(extrema(z),digits=1); 
     contour(x,y,z/max(-mn,mx),levels=filter(≠(0),-0.9:0.15:0.9),aspect_ratio=:equal,
         title="Point-source wave elevation, Fn=$Fn, ζ/Fn²=[$mn,$mx]",legend=false)
+end
+source_elevation_plot()
+
+plt = plot(aspect_ratio=:equal)
+@gif for Fn = 1:-0.05:0.2
+    ζ(x,y) = derivative(x->kelvin([x,y,0],[0,0,-1];Fn),x) # unit-point source
+    x,y = -15:0.1:3,-6:0.1:6; z = ζ.(x',y)/2.5
+    contour(x,y,z,levels=filter(≠(0),-0.9:0.15:0.9),title="Point-source wave elevation, Fn=$Fn",legend=false)
 end
