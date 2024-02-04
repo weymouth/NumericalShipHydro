75368bb5bb91f7934b0c33617312723e8b77f513
weymouth <gabriel.weymouth@gmail.com>
2024-02-04 14:42:19 +0100

    Finished N integration
    Since the amplitude of the branch discontinuity is also an exp((1+T^2)z) term, I reused the same logic that was working for W to clamp T₀ between -b,b. I defined ltol as the log-tolerance setting b, and this works beautifully. Roughly speaking, if you set ltol=-8log(10), you'll need 80 GL points to avoid missing the peaks in both integrals. The results look also identical with ltol=-3log(10) using 30 points, but I've left the default at ltol=-5log(10) and 50 points for now.

--- src/kelvin.jl
+++ src/kelvin.jl
@@ -1,22 +1,23 @@
 include("solve.jl")
 """
-    kelvin(ξ,a;Fn=1,kwargs...)
+    kelvin(ξ,a;Fn=1,ltol=-5log(10),kwargs...)
 
 Green Function `G(ξ)` for a source at position `α` moving with `Fn≡U/√gL` below 
 the free-surface. This function assumes that the free surface location is ζ=0 
 and the apparent velocity direction is Û=[-1,0,0]. See Noblesse 1981 for details.
+Smaller log-tolerance `ltol` will only reduce errors when using a large number of
+Gauss-Legendre points. Otherwise, it leads to instability.
 """
-function kelvin(ξ,α;Fn=1,kwargs...)
+function kelvin(ξ,α;Fn=1,ltol=-5log(10),kwargs...)
     α[3] ≥ 0 && throw(DomainError(α[3],"Source must be below the free surface at ζ=0"))
     # Froude number scaled distances from the source's image
     x,y,z = (ξ-α .* [1,1,-1])/Fn^2
-    # Near-field image and non-oscillatory wave
-    T₀ = ifelse(y==0,0,exp(0.01z*(1+x^2/y^2))*x/y) # integration center
-    S = max(abs(T₀),1)                             # integration scale
-    N = 1/hypot(x,y,z)+2S/π*quadgl_inf(T->Ni(x,y,z,S*T-T₀);kwargs...)
     # Far-field oscillatory wave
-    b = √max(-5log(10)/z-1,0); a = max(x/abs(y),-b) # integration limits
+    b = √max(ltol/z-1,0); a = max(x/abs(y),-b) # integration limits
     W = ifelse(a≥b || x==y==0, 0., 4*quadgl_ab(T->Wi(x,y,z,T),a,b;kwargs...))
+    # Near-field image and non-oscillatory wave
+    T₀ = ifelse(y==0,0,clamp(x/y,-b,b)); S = max(abs(T₀),π) # integral center & scale
+    N = 1/hypot(x,y,z)+2S/π*quadgl_inf(T->Ni(x,y,z,S*T-T₀);kwargs...)
     # Total Green function
     return source(ξ,α)+(N+W)/Fn^2
 end
@@ -24,8 +25,8 @@ Ni(x,y,z,T) = real(expintx(complex((1+T^2)*z,eps(T)+(x+y*T)*hypot(1,T))))
 Wi(x,y,z,T) = exp((1+T^2)*z)*sin((x-abs(y)*T)*hypot(1,T))
 
 using Plots
-function source_elevation_plot(;Fn=1) # test function 
-    ζ(x,y) = derivative(x->kelvin([x,y,0],[0,0,-1];Fn),x) # unit-point source
+function source_elevation_plot(;Fn=1,kwargs...) # test function 
+    ζ(x,y) = derivative(x->kelvin([x,y,0],[0,0,-1];Fn,kwargs...),x) # unit-point source
     x,y = -15:0.1:3,-6:0.1:6; z = ζ.(x',y)
     mn,mx = round.(extrema(z),digits=1); 
     contour(x,y,z/max(-mn,mx),levels=filter(≠(0),-0.9:0.15:0.9),aspect_ratio=:equal,
